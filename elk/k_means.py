f430e9fe4ad66a970708f397e8bb6cb08cd4cac2
Walter Laurito <Lauritowal@yahoo.com>
2023-10-14 18:07:13 +0200

    get train and val dict

--- elk/k_means.py
+++ elk/k_means.py
@@ -4,23 +4,40 @@ from dataclasses import dataclass
 from simple_parsing.helpers import Serializable
 
 from elk.extraction import Extract, extract
+from elk.utils.data_utils import prepare_data
+from elk.utils.gpu_utils import get_device
+
+from .utils import (
+    select_usable_devices,
+)
 
 
 @dataclass
 class Kmeans(ABC, Serializable):
     data: Extract
+    num_gpus: int = 4
+    world_size: int = 1
+    min_gpu_mem: int | None = None
+    prompt_indices: tuple[int, ...] = ()
 
     def execute(self):
         datasets = [
             extract(
                 cfg,
                 disable_cache=False,
-                num_gpus=4,
+                num_gpus=self.num_gpus,
             )
             for cfg in self.data.explode()
         ]
+        devices = select_usable_devices(self.num_gpus, min_memory=self.min_gpu_mem)
 
-        print("datasets", datasets)
+        layer = 1
+        device = get_device(devices, self.world_size)
+        train_dict = prepare_data(datasets, device, layer, "train")
+        val_dict = prepare_data(datasets, device, layer, "val")
+
+        print("train_dict", train_dict)
+        print("val_dict", val_dict)
 
 
 # X = np.array([[1, 2], [1, 4], [1, 0],
