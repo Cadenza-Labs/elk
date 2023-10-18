a68d3eeb4e8ccf35a9154e0e51d9d56f54b3fdc5
Walter Laurito <Lauritowal@yahoo.com>
2023-10-18 17:12:55 +0200

    get clusters in dict

--- elk/k_means.py
+++ elk/k_means.py
@@ -26,13 +26,38 @@ class Kmeans(ABC, Serializable):
     min_gpu_mem: int | None = None
     prompt_indices: tuple[int, ...] = ()
 
+    def get_clusters(self, X: torch.Tensor, cluster_ids: list) -> dict:
+        """
+        Create a dictionary where each key is a unique cluster ID and
+        the associated value is a list of
+        data points (from X) that belong to that cluster.
+
+        Parameters:
+        - X (torch.Tensor): A tensor containing the data points.
+        - cluster_ids (list): A list containing the cluster ID
+        for each data point in X.
+
+        Returns:
+        - dict: A dictionary with unique cluster IDs as keys
+        and lists of data points as values.
+        """
+        clusters = {}
+        for unique_cluster_id in list(set(cluster_ids)):
+            clusters[unique_cluster_id] = []
+
+        for idx, cluster_id in enumerate(cluster_ids):
+            clusters[cluster_id].append(X[idx])
+
+        return clusters
+
     def apply(self, X, k=10):
         cluster_ids, cluster_centers = kmeans(
-            X=X, num_clusters=k, distance="euclidean", device=torch.device("cuda:0")
+            X=X,
+            num_clusters=k,
+            distance="euclidean",
+            device=torch.device("cuda:0"),  # TODO: make it work for more than one GPU
         )
-        print(cluster_ids)
-        print(cluster_centers)
-        breakpoint()
+        self.get_clusters(X, cluster_ids.tolist())
 
     def execute(self):
         datasets = [
@@ -48,13 +73,11 @@ class Kmeans(ABC, Serializable):
         layer = 1
         device = get_device(devices, self.world_size)
         train_dict = prepare_data(datasets, device, layer, "train")
-        val_dict = prepare_data(datasets, device, layer, "val")
+        prepare_data(datasets, device, layer, "val")
 
         hiddens = train_dict["imdb"][0]
         averaged_over_choices = hiddens.mean(dim=2)
         n, v, d = averaged_over_choices.shape
         reshaped = averaged_over_choices.view(n * v, d)
-        self.apply(X=reshaped, k=v)
-
-        print("train_dict", train_dict)
-        print("val_dict", val_dict)
+        cluster_data = self.apply(X=reshaped, k=v)
+        print(cluster_data)
