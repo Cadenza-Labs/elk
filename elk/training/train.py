01d30fe564f52628eba89d079c5881d4dbef41e3
jon <jonathan.n@u.nus.edu>
2023-10-26 11:45:29 +0000

    running projection thing

--- elk/training/train.py
+++ elk/training/train.py
@@ -247,25 +247,33 @@ class Elicit(Run):
                             )
                         )
                     assert pseudolabel_directions.shape == (v, d)
-                    U, S, V = torch.linalg.svd(pseudolabel_directions.T)
+                    U, S, V = torch.linalg.svd(
+                        pseudolabel_directions.T, full_matrices=False
+                    )
                     to_save[f"6_{self.name}_singular_values"] = (
                         S.detach().cpu().numpy().tolist()
                     )
-                    probe = TPCProbe.train(hiddens, correct_norm=False)
+                    probe = TPCProbe.train(hiddens, correct_norm=True)
                     probe_direction = probe.probe_direction.squeeze(-1)
+
                     Q, R = torch.linalg.qr(pseudolabel_directions.T)
-                    # top 3 right singular vectors
                     projection = Q @ Q.T @ probe_direction
-                    other_projection = U[:3] @ U[:3].T @ probe_direction
+                    other_projection = U[:, :3] @ U[:, :3].T @ probe_direction
                     projection_len = torch.linalg.vector_norm(projection) ** 2
-                    other_projection_len = torch.linalg.vector_norm(other_projection) ** 2
+                    other_projection_len = (
+                        torch.linalg.vector_norm(other_projection) ** 2
+                    )
                     probe_len = torch.linalg.vector_norm(probe_direction) ** 2
                     to_save[f"6_{self.name}_z_projection_probe_ratio"] = (
-                        projection_len / probe_len
-                    ).detach().cpu().numpy().tolist()
+                        (projection_len / probe_len).detach().cpu().numpy().tolist()
+                    )
                     to_save[f"6_{self.name}_z_other_projection_probe_ratio"] = (
-                        other_projection_len / probe_len
-                    ).detach().cpu().numpy().tolist()
+                        (other_projection_len / probe_len)
+                        .detach()
+                        .cpu()
+                        .numpy()
+                        .tolist()
+                    )
 
             Expt(hiddens, lambda x: x, "no_norm").run()
             Expt(hiddens, lambda x: norm(x, wrong=False), "correct_norm").run()
