c036a29c9c387fe69ce8158f8a07e9e03bd298c3
Walter Laurito <Lauritowal@yahoo.com>
2024-02-14 06:49:35 +0100

    add sweep

---
+++ sweep-kmeans-template-2-k-8.sh
@@ -0,0 +1,2 @@
+elk sweep --template_path /home/laurito/elk/scripts/templates/imdb/2/ --net ccs --norm cluster --cluster_algo kmeans --k_clusters 8 --models EleutherAI/pythia-12b EleutherAI/pythia-6.9b EleutherAI/pythia-12b meta-llama/Llama-2-7b-hf meta-llama/Llama-2-13b-hf openai-community/gpt2-medium openai-community/gpt2-large "mistralai/Mistral-7B-v0.1" --datasets imdb   --skip_transfer_eval  --num_gpus 2 --disable_cache
+git rev-parse cadenza/k-means
