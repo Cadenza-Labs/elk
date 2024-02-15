a86a3f1ce22286354c83a930f5cae6d54f5fda4f
Walter Laurito <Lauritowal@yahoo.com>
2024-02-16 00:08:52 +0100

    add sweeps

---
+++ sweep-template-32-hdbscan.sh
@@ -0,0 +1,4 @@
+export OMP_NUM_THREADS=20
+echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
+elk sweep --template_path /home/laurito/elk/scripts/templates/imdb/32/ --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 4 --disable_cache
+git rev-parse cadenza/k-means
