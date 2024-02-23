d741b9d08b8b3e1b8d77d35824af7bfe0550f1db
Walter Laurito <Lauritowal@yahoo.com>
2024-02-23 02:49:59 +0100

    add seed 43

--- sweep-HDBSCAN-template-8.sh
+++ sweep-HDBSCAN-template-8.sh
@@ -1,2 +1,2 @@
-elk sweep --template_path /home/laurito/elk/scripts/templates/train/imdb/64/ --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb   --skip_transfer_eval  --num_gpus 6 #--disable_cache
+elk sweep --seed 43 --template_path /home/laurito/elk/scripts/templates/train/imdb/8/ --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb   --skip_transfer_eval  --num_gpus 6 #--disable_cache
 git rev-parse cadenza/k-means
--- sweep-template-32-kmeans-k32.sh
+++ sweep-template-32-kmeans-k32.sh
@@ -1,4 +1,2 @@
-#export OMP_NUM_THREADS=20
-#echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
 elk sweep --template_path /home/laurito/elk/scripts/templates/imdb/32/ --net ccs --norm cluster --cluster_algo kmeans --k_clusters 32 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 6 # --disable_cache
 git rev-parse cadenza/k-means
