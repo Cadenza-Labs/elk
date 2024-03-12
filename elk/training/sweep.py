ce6c1714905bde65b73a7551bac2c4d780b5e3b0
Walter Laurito <Lauritowal@yahoo.com>
2024-03-12 16:34:14 +0100

    revert sweep stuff

--- elk/training/sweep.py
+++ elk/training/sweep.py
@@ -54,16 +54,14 @@ class Sweep:
     name: str | None = None
 
     # A bit of a hack to add all the command line arguments from Elicit
-    run_template: Elicit = None
+    run_template = Elicit(
+        data=Extract(
+            model="<placeholder>",
+            datasets=("<placeholder>",),
+        )
+    )
 
     def __post_init__(self, add_pooled: bool):
-        if self.run_template is None:
-            self.run_template = Elicit(
-                data=Extract(
-                    model="<placeholder>",
-                    datasets=("<placeholder>",),
-                )
-            )
         if not self.datasets:
             raise ValueError("No datasets specified")
         if not self.models:
