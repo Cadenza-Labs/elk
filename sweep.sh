c8dc5013bc32f4ce569de0bf484843ca2cb96995
Walter Laurito <Lauritowal@yahoo.com>
2024-03-12 19:19:57 +0100

    update sweep

--- sweep.sh
+++ sweep.sh
@@ -1,2 +1,2 @@
-elk sweep --num_gpus 4 --models EleutherAI/pythia-12b EleutherAI/pythia-6.9b EleutherAI/pythia-12b --datasets imdb amazon_polarity meta-llama/Llama-2-7b-hf meta-llama/Llama-2-13b-hf openai-community/gpt2-medium openai-community/gpt2-large "mistralai/Mistral-7B-v0.1"   --datasets imdb amazon_polarity   --skip_transfer_eval --net ccs --norm cluster  --cluster_algo kmeans  --k_clusters 100
+elk sweep --seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net ccs --norm cluster --cluster_algo hdbscan --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 6 # --disable_cache
 git rev-parse cadenza/k-means
