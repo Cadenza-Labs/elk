3dcd9837733a03ae0395118b16f4c600c5ada248
Walter Laurito <Lauritowal@yahoo.com>
2024-03-19 16:00:15 +0100

    cleanup

--- elk/training/train.py
+++ elk/training/train.py
@@ -6,7 +6,6 @@ from typing import Literal
 
 import pandas as pd
 import torch
-from einops import rearrange, repeat
 from simple_parsing import subgroups
 from sklearn.cluster import HDBSCAN, KMeans, SpectralClustering
 
@@ -17,7 +16,7 @@ from elk.utils.data_utils import prepare_data
 from elk.utils.gpu_utils import get_device
 
 from ..evaluation import Eval
-from ..metrics import evaluate_preds, to_one_hot
+from ..metrics import evaluate_preds
 from ..metrics.eval import LayerOutput
 from ..run import LayerApplied, Run
 from ..training.supervised import train_supervised
@@ -25,7 +24,7 @@ from ..utils.types import PromptEnsembling
 from . import Classifier
 from .ccs_reporter import CcsConfig, CcsReporter
 from .common import FitterConfig
-from .eigen_reporter import EigenFitter, EigenFitterConfig
+from .eigen_reporter import EigenFitterConfig
 from .multi_reporter import MultiReporter, ReporterWithInfo, SingleReporter
 
 DEEPMIND_REPRODUCTION = False
@@ -506,60 +505,23 @@ class Elicit(Run):
     def train_and_save_reporter(
         self, device, layer, out_dir, train_dict, prompt_index=None
     ) -> ReporterWithInfo:
-        (first_train_h, train_gt, _, _), *rest = train_dict.values()  # TODO can remove?
-
-        (_, v, k, d) = first_train_h.shape
-        if not all(other_h.shape[-1] == d for other_h, _, _ in rest):
-            raise ValueError("All datasets must have the same hidden state size")
+        dataset_key = list(train_dict.keys())[0]
+        hiddens = train_dict[dataset_key][0]
+        labels = train_dict[dataset_key][1]
+        (_, v, k, d) = hiddens.shape
 
         if DEEPMIND_REPRODUCTION:
-            first_train_h, train_gt = deepmind_reproduction(first_train_h, train_gt)
-
-        # For a while we did support datasets with different numbers of classes, but
-        # we reverted this once we switched to ConceptEraser. There are a few options
-        # for re-enabling it in the future but they are somewhat complex and it's not
-        # clear that it's worth it.
-        if not all(other_h.shape[-2] == k for other_h, _, _ in rest):
-            raise ValueError("All datasets must have the same number of classes")
+            hiddens, labels = deepmind_reproduction(hiddens, labels)
 
         train_loss = None
         if isinstance(self.net, CcsConfig):
             assert len(train_dict) == 1, "CCS only supports single-task training"
-            (_, v, k, d) = first_train_h.shape
+            (_, v, k, d) = hiddens.shape
             reporter = CcsReporter(self.net, d, device=device)
 
             # pca_visualizations(layer, first_train_h, train_gt, out_dir=self.out_dir)
 
-            train_loss = reporter.fit(first_train_h)
-
-            labels = repeat(to_one_hot(train_gt, k), "n k -> n v k", v=v)
-            reporter.platt_scale(labels, first_train_h)
-
-        elif isinstance(self.net, EigenFitterConfig):
-            fitter = EigenFitter(
-                self.net, d, num_classes=k, num_variants=v, device=device
-            )
-
-            hidden_list, label_list = [], []
-            for ds_name, (train_h, train_gt, _, _) in train_dict.items():
-                (_, v, _, _) = train_h.shape
-
-                # Datasets can have different
-                # numbers of variants, so we need to
-                # flatten them here before concatenating
-                hidden_list.append(rearrange(train_h, "n v k d -> (n v k) d"))
-                label_list.append(
-                    to_one_hot(repeat(train_gt, "n -> (n v)", v=v), k).flatten()
-                )
-                fitter.update(train_h)
-
-            reporter = fitter.fit_streaming()
-            reporter.platt_scale(
-                torch.cat(label_list),
-                torch.cat(hidden_list),
-            )
-        else:
-            raise ValueError(f"Unknown reporter config type: {type(self.net)}")
+            train_loss = reporter.fit(hiddens)
 
         # Save reporter checkpoint to disk
         # TODO have to change this
@@ -608,7 +570,7 @@ class Elicit(Run):
                 # concatenate train and val hiddens and save the result in hiddens
                 hiddens = train_dict[dataset_name][0]
                 labels = train_dict[dataset_name][1]
-                train_dict[dataset_name][2]
+                # train_dict[dataset_name][2]
                 text_questions = train_dict[dataset_name][3]
 
                 _, v, _, _ = train_dict[dataset_name][0].shape
@@ -629,11 +591,6 @@ class Elicit(Run):
                 )
                 clusters_by_dataset[dataset_name] = clusters
 
-                # print("train viz")
-                # visualize_clusters(clusters["train"])
-                # print("test viz")
-                # visualize_clusters(clusters["test"])
-
                 save_clusters_representations(
                     dataset_name, clusters, "train", self.out_dir, layer
                 )
@@ -653,13 +610,13 @@ class Elicit(Run):
             )
         else:
             if probe_per_prompt:
-                (first_train_h, train_gt, _), *rest = train_dict.values()
-                (_, v, k, d) = first_train_h.shape
+                hiddens = train_dict["imdb"][0]
+                labels = train_dict["imdb"][1]
+
+                (_, v, k, d) = hiddens.shape
 
                 if DEEPMIND_REPRODUCTION:
-                    first_train_h, train_gt = deepmind_reproduction(
-                        first_train_h, train_gt
-                    )
+                    hiddens, labels = deepmind_reproduction(hiddens, labels)
 
                 # self.prompt_indices being () actually means "all prompts"
                 prompt_indices = (
@@ -669,11 +626,11 @@ class Elicit(Run):
                     {
                         ds_name: (
                             train_h[:, [i], ...],
-                            train_gt,
+                            labels,
                             lm_preds[:, [i], ...] if lm_preds is not None else None,
                         )
                     }
-                    for ds_name, (train_h, _, lm_preds) in train_dict.items()
+                    for ds_name, (train_h, _, lm_preds, _) in train_dict.items()
                     for i, _ in enumerate(prompt_indices)
                 ]
 
