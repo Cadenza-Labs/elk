b6091ed765aa8ea5aba3c178701fc3abbab670f6
Walter Laurito <Lauritowal@yahoo.com>
2024-04-16 13:56:30 +0200

    adapt sweep

--- sweep.sh
+++ sweep.sh
@@ -1,5 +1,3 @@
-# elk sweep --net.seed 44 --template_path /home/laurito/elk/scripts/templates/implicit_opinion/train --net ccs --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets fancyzhx/dbpedia_14 --skip_transfer_eval --num_gpus 2 --max_examples 3000 3000 --disable_cache
-# git rev-parse cadenza/main
-
-
-elk eval  /home/wombat_share/laurito/elk_reporters/sweeps/zealous-wilson/mistralai/Mistral-7B-v0.1/fancyzhx/dbpedia_14/ mistralai/Mistral-7B-v0.1 fancyzhx/dbpedia_14 --template_path /home/laurito/elk/scripts/templates/implicit_opinion/eval --num_gpus 1 --norm False
+# elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/implicit_opinion/train --net ccs --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets "imdb" --skip_transfer_eval  --num_gpus 2 --disable_cache
+elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/implicit_opinion/train --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets "imdb" --skip_transfer_eval  --num_gpus 2 --disable_cache
+git rev-parse cadenza/k-means
