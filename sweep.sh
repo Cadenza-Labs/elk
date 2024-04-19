3cb961e24bf0bd65c8a92cb81aca33639adde8a9
Walter Laurito <Lauritowal@yahoo.com>
2024-04-19 17:19:58 +0200

    templates

--- sweep.sh
+++ sweep.sh
@@ -1,7 +1,11 @@
-elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/personas/train/truthfulqa/ --net ccs --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets EleutherAI/truthful_qa_mc --skip_transfer_eval --num_gpus 2 --disable_cache
-
-
-# elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval  --num_gpus 6 # --disable_cache
-# git rev-parse cadenza/k-means
-
-# elk sweep --template_path /home/laurito/elk/scripts/templates/random/train/imdb/1 --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 2  --disable_cache
+elk sweep --net.seed 42 \
+    --template_path /home/laurito/elk/scripts/templates/personas/train/claims/ \
+    --net ccs \
+    --norm burns \
+    --cluster_algo None \
+    --dataset_from_file True \
+    --models "mistralai/Mistral-7B-v0.1" \
+    --datasets "/home/laurito/elk/datasets/claim" \
+    --skip_transfer_eval \
+    --num_gpus 1 \
+    --disable_cache
