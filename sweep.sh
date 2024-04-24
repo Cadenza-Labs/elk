b3b11ec7beea61eee25f74bad6ab8cb3f383a4e3
Walter Laurito <Lauritowal@yahoo.com>
2024-04-25 01:33:18 +0200

    add sweep

--- sweep.sh
+++ sweep.sh
@@ -2,11 +2,11 @@
 git rev-parse cadenza/k-means
 
 # cluster norm
-elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 2 --disable_cache
-elk sweep --net.seed 43 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 2 --disable_cache
-elk sweep --net.seed 44 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 2 --disable_cache
+elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 #--disable_cache
+elk sweep --net.seed 43 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 # --disable_cache
+elk sweep --net.seed 44 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 # --disable_cache
 
 # deepmind no templates norm
-elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 6 # --disable_cache
-elk sweep --net.seed 43 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 6 # --disable_cache
-elk sweep --net.seed 44 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 6 # --disable_cache
+elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 # --disable_cache
+elk sweep --net.seed 43 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 # --disable_cache
+elk sweep --net.seed 44 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net crc --norm burns --cluster_algo None  --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval --num_gpus 8 # --disable_cache
