540cbbf76403993f1613a94b3d311e5fdb379803
Walter Laurito <Lauritowal@yahoo.com>
2024-05-16 00:53:46 +0200

    update templates

--- sweep.sh
+++ sweep.sh
@@ -1,5 +1,5 @@
 # elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/personas/train/claims/ --net ccs --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets lauritowal/claims --skip_transfer_eval --num_gpus 1 # --disable_cache
-elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/personas/train/truthfulqa/ --net ccs --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets lauritowal/truthful_qa --skip_transfer_eval --num_gpus 1 # --disable_cache
+elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/personas/train/truthfulqa/ --net crc --norm burns --cluster_algo None --models "mistralai/Mistral-7B-v0.1" --datasets lauritowal/truthful_qa --skip_transfer_eval --num_gpus 2 --disable_cache
 
 
 # elk sweep --net.seed 42 --template_path /home/laurito/elk/scripts/templates/explicit_opinion/train/ --net ccs --norm cluster --cluster_algo HDBSCAN --min_cluster_size 5 --models "mistralai/Mistral-7B-v0.1" --datasets imdb --skip_transfer_eval  --num_gpus 6 # --disable_cache
